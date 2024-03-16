8e996739f575e921a1f02daa1e7badc581d47659
Muhammad Yasir <154784215+YasirBrothers@users.noreply.github.com>
2024-03-16 19:44:01 +0500

    Add files via upload

---
+++ Js-4/app.js
@@ -0,0 +1,42 @@
+
+// Chapter # 13-15
+
+// Q1   
+//     var studentName = [];
+
+//    console.log(studentName);
+
+ Q2
+    // var futureStudents = {
+    //     names: []
+    // }
+
+    // console.log{futureStudents};
+
+
+
+ // Q3
+    // var stringsArray = ["Pakistan", "Turkey", "Saudia"];
+
+    // console.log(stringsArray);
+
+
+
+// Q4
+    // var stringsArray = [1,2,3,4,5];
+    
+    // console.log(stringsArray);
+
+//  Q5
+    // var booleanArray = [true, false, true, false];
+
+    // console.log(booleanArray);
+
+
+//  Q6
+    // var mixedArray = ["string", 123, true, { key: "value" }];
+
+    // console.log(mixedArray);
+
+// Q7
+   
