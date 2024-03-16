852eb1b33c83f553de25e28d33297e58e7bd8d64
Muhammad Yasir <154784215+YasirBrothers@users.noreply.github.com>
2024-03-17 00:24:52 +0500

    Update app.js

--- Js-4/app.js
+++ Js-4/app.js
@@ -6,12 +6,12 @@
 
 //    console.log(studentName);
 
- Q2
+// Q2
     // var futureStudents = {
     //     names: []
     // }
 
-    // console.log{futureStudents};
+  
 
 
 
@@ -34,9 +34,74 @@
 
 
 //  Q6
-    // var mixedArray = ["string", 123, true, { key: "value" }];
+    // var mixedArray = ["string", 123, true, undefined, Yasir];
 
     // console.log(mixedArray);
 
+
 // Q7
+    //   var education = ["SSC","HSC","BCS","BS","BCOM","MS","M.Phil","PhD"];
+
+    //   console.log("1)", education[0]);
+    //   console.log("2)", education[1]);
+    //   console.log("3)", education[2]);
+    //   console.log("4)", education[3]);
+    //   console.log("5)", education[4]);
+    //   console.log("6)", education[5]);
+    //   console.log("7)", education[6]);
+    //   console.log("8)", education[7]);
+
+
    
+
+
+// Q8
+//   var students = ["Yasir", "Saad", "Ibraheem"];
+
+//   var scores = [320,230,480];
+
+//   var student1 = (scores[0] /500) * 100;
+//   var student2 = (scores[1] /500) * 100;
+//   var student3 = (scores[2] /500) * 100;
+
+//   console.log("Score of", students[0], "is", scores[0],  "Percentage :", student1 + "%");
+//   console.log("Score of", students[1], "is", scores[1],  "Percentage :", student2 + "%");
+//   console.log("Score of", students[2], "is", scores[2],  "Percentage :", student3 + "%");
+   
+
+
+
+// Q9
+    // var Arraycolors = ["Green", "Blue", "Orange", "Brown", "Purple"];
+
+    // console.log("Names of Color is :", Arraycolors);
+
+// Part-a
+//     var userAddColor = prompt("Please Enter a Color to add Beginning");
+//     Arraycolors.unshift(userAddColor);
+//     console.log("Names of color is", Arraycolors);
+
+//  Part-b
+//     var userAddColor = prompt("Please Enter a Color to add Ending");
+//     Arraycolors.push(userAddColor);
+//     console.log("Names of color is", Arraycolors);
+
+// Part-c
+//     Arraycolors.unshift("Black", "Grey");
+//     console.log("Names of color is", Arraycolors);
+
+// Part-d
+    // var deleteColorFirst = Arraycolors.shift();
+    // console.log("Deleted Color First =", deleteColorFirst);
+    // console.log("Names of color is", Arraycolors);
+
+// Part-e
+    // var deleteColorEnd = Arraycolors.pop();
+    // console.log("Deleted Color End =", deleteColorEnd);
+    // console.log("Names of color is", Arraycolors);
+
+// Part-f
+
+// Part-g
+
+
