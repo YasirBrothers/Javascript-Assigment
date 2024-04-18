f122312eb61202ac7b465597a2408cfdeec3b104
Muhammad Yasir <154784215+YasirBrothers@users.noreply.github.com>
2024-04-19 00:43:33 +0500

    Add files via upload

---
+++ Js-6/app.js
@@ -0,0 +1,25 @@
+// Assigment 6
+
+// Chapter# 21 to 25
+
+
+
+// Q1
+    // var firstname = prompt("Please Enter Your First Name");
+    // var lastname  = prompt("Please Enter Your Last Name");
+
+    // console.log(firstname + lastname);
+
+
+// Q2
+    //  var favoritemobile = prompt("What is your favorite mobile phone model?");
+    //  var lenghtofinput = favoritemobile.length;
+  
+    //  console.log("Length of your favorite mobile phone model input is: " + lenghtofinput);
+
+
+// Q3
+    // var word = prompt("Please Enter a word to showing 'n' alphabet");
+    // var indexOfN = word.indexOf("n");
+
+    // console.log("The index of letter 'n' in the word is: " + indexOfN);
