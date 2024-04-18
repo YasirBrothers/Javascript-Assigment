29c731687a8fefa1309e737b993f877a87228ffc
Muhammad Yasir <154784215+YasirBrothers@users.noreply.github.com>
2024-04-19 01:11:41 +0500

    Add files via upload

---
+++ Js-5/app.js
@@ -0,0 +1,33 @@
+// Assignment : 5
+
+// Chapter# 17 to 20
+
+// Q1
+    // let multiArray = [[]];    
+    // let multiArray = [[],[]];
+
+    
+ // Q2
+    // let multiArray = [
+    //                  [0,1,2,3],
+    //                  [1,0,1,2],
+    //                  [2,1,0,1]
+    //                  ];
+    
+    // console.log(multiArray[0]);
+    // console.log(multiArray[1]);  
+    // console.log(multiArray[2]);
+
+
+// Q3
+    // for (var i=1; i<11; i++) {
+    //     console.log(i);
+    // }
+
+
+// Q4
+
+
+// Q5
+    var fruits = ["apple","banana","mango","orange","strawberry"];
+    console.log(fruits);
