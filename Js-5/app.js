9da4bbb5c82222add46ad3386c75ee3299d10a23
Muhammad Yasir <154784215+YasirBrothers@users.noreply.github.com>
2024-04-19 15:08:41 +0500

    Update app.js

--- Js-5/app.js
+++ Js-5/app.js
@@ -29,5 +29,57 @@
 
 
 // Q5
-    var fruits = ["apple","banana","mango","orange","strawberry"];
-    console.log(fruits);
+   //    let fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+//     for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+//     }
+
+//     for (let i = 0; i < fruits.length; i++) {
+//     console.log("Element at index", i, "is", fruits[i]);
+//     }
+
+
+// Q6
+    // console.log("a. Counting:");
+    // for (let i = 1; i <= 15; ++i) {
+    //     console.log(i);
+    // }
+
+    // console.log("a. Reverse Counting:");
+    //  for (let i = 10; i > 0; i--) {
+    //     console.log(i);
+    //  }
+
+    // console.log("a. Even:");
+    // for (let i = 0; i <= 20; i += 2 ) {
+    //     console.log(i);
+    // }
+    
+    // console.log("a. Odd:");
+    // for (let i = 1; i <=19; i +=2) {
+    //     console.log(i)
+    // } 
+
+    // console.log("a. Series:");
+    // for (let i = 2; i <= 20; i += 2 ) {
+    //     console.log(i + "k,") ;
+    // }
+    
+
+// Q7 
+    
+
+
+// Q8
+
+
+// Q9
+
+
+// Q10
+    // console.log("Multiples of 5 Ranging:");
+    // for (let i = 5; i <= 100; i += 5 ) {
+    //     console.log(i);
+    // }
+    
