f7d235b11e57a520129bc1e2057d902bb1806c0b
Muhammad Yasir <154784215+YasirBrothers@users.noreply.github.com>
2024-04-21 23:40:55 +0500

    Add files via upload

---
+++ Js-2/app.js
@@ -0,0 +1,247 @@
+// Chapter#4
+
+// Q1
+//  var name = "My name is Muhammad Yasir.";
+//  alert(name);
+
+//  var age  = "My age is 23 years old. ";
+//  alert(age);
+
+//  var year = "My Birth year is 2000.";
+//  alert(year);
+
+// Q2(a)
+//  console.log("Legal Variables Names:")
+
+// console.log("(1)useralert")
+// console.log("(2)heading 1");
+// console.log("(3)fullName");
+// console.log("(4)full_name");
+// console.log("(5)userdocument");
+
+// Q2(b)
+// console.log("Illegal Variable Names:") 
+
+// console.log("(1)alert")
+// console.log("(2)1 number");
+// console.log("(3)document");
+// console.log("(4)fullnamee");
+// console.log("(5)full-name");
+
+// Q3
+//  document.write("<h1>Rules for naming JS variables </h1>");
+//  document.write("<p> Variables names can only contain .number,$ and_. For example: $my_1st Variable<br> Variable must begin with a letters, $ or_,For example: $name or name <br> Variable names are case sensitive <br> Variable names should not be JS keyword </p>");
+
+
+// Chapter#5
+
+//  Q1
+//  var num1 = 3;
+//  var num2 = 5;
+
+//  console.log(num1 + num2);
+
+// Q2
+// var num1 =3;
+// var num2 =5;
+
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
+
+// Q3
+
+// console.log("Q3 Part(a)");
+//  var age;
+//  age = 10;
+//  console.log(age);
+
+//  console.log("Q3 Part(b)");
+//  console.log("Value after declaration is "+age)
+
+// console.log("Q3 Part(c & d)");
+//  var number = 5;
+
+// console.log("Initial value "+number)
+
+// console.log("Q3 Part(e & f)");
+//  number++
+
+//  console.log(number);
+
+//  console.log("Q3 Part(g & h)");
+// console.log(number + 7);
+
+
+
+
+
+//  number += 7;
+
+//  number = number + 7
+
+//  console.log(number);
+
+//  number--
+
+//  console.log(number);
+
+//  number = number % 3
+
+//  console.log(number);
+
+
+//  Q4
+//  var num1 = 5;
+//  var num2 = 600;
+
+//  console.log("total cost of 5 tickets is" +  num1 * num2);
+
+
+// Q5
+// var num = 4;
+// var num1 = 1;
+// var num2 = 2;
+// var num3 = 3;
+// var num4 = 4;
+// var num5 = 5;
+// var num6 = 6;
+// var num7 = 7;
+// var num8 = 8;
+// var num9 = 9;
+// var num10 = 10;
+
+// console.log(num*num1);
+// console.log(num*num2);
+// console.log(num*num3);
+// console.log(num*num4);
+// console.log(num*num5);
+// console.log(num*num6);
+// console.log(num*num7);
+// console.log(num*num8);
+// console.log(num*num9);
+// console.log(num*num10);
+
+
+// Q6
+
+// // Q7
+// document.write("<h1>Shopping Cart</h1>");
+// document.write("<h4>Price of item 1 is 650</h4>");
+// document.write("<h4>Quantity of item 1 is 3</h4>");
+// document.write("<h4>Price of item 2 is 100</h4>");
+// document.write("<h4>Quantity of item 2 is 7</h4>");
+// document.write("<h4>Shipping charges is 100</h4>");
+
+// var price1 = 650
+// var qty1 = 3
+// var price2 = 100
+// var qty2 = 7
+// var charges = 100
+
+// console.log("Total Cost of your order = " +(price1*qty1)+(price2*qty2)+100);
+
+// Q8
+// document.write("<h1>Marksheet</h1>");
+// document.write("<h4>Total Marks is 980</h4>");
+// document.write("<h4>Marks Obtained is 804</h4>");
+
+// var total = 980
+// var obtain = 804
+// var multiply = 100
+
+// console.log("Percentage is = "+(obtain*multiply)/total);
+
+
+// Q9
+// document.write("<h1>Curreny in PKR<h1>");
+// document.write("<h4>Us dollar is 10</h4>");
+// document.write("<h4>Saudi Riyal is 25 </h4>");
+// document.write("<h4>1 dollar cost in Pakistani = 104.80 </h4>");
+// document.write("<h4>1 Saudi Riyal cost in Pakistan = 28</h4>");
+
+// var dollar = 104.80 
+// var qty = 10
+// var saudi = 28
+// var qty1 = 25
+
+// console.log((dollar*qty)+(saudi*qty1));
+
+
+// Q10
+// var num1 = 5
+// var num2 = 10
+// var num3 = 2
+
+// console.log((num1*num2)/num3);
+
+
+// Q11
+// document.write("<h1>Age Calculator<h1>");
+//  document.write("<h4>Current year is 2024</h4>");
+//  document.write("<h4>Birth Year is 2000</h4>");
+ 
+// var year = 2024
+// var birth = 2000
+
+//  console.log(year-birth);
+
+
+// Q12
+// var radius = 20
+// var pye = 3.142
+
+
+// console.log(2*(radius*pye));
+// console.log((radius*pye)*(2*10));
+
+
+// Q13
+
+
+// Chapter#6
+
+// Q1
+// var a = 10
+
+// console.log("The value of ++a is = "+ (++a));
+// console.log("The value of a is = "+(a));
+
+// console.log("The value of a++ is = "+(a++));
+// console.log("The value of a is = "+(a));
+
+// console.log("The value of --a is = "+ (--a));
+// console.log("The value of a is = "+(a));
+
+// console.log("The value of a-- is = "+(a--));
+// console.log("The value of a is = "+(a));
+
+// Q2
+//  var a = 2
+//  var b = 1
+//  console.log("The value of --a is = "+ (--a));
+//  console.log("The value of --a - --b is = "+(--a - --b));
+
+//  console.log("The value of --a - --b + ++b is = "+(--a - --b + ++b));
+//  console.log("The value of --a - --b + ++b + b-- is = "+(--a - --b + ++b + b--));
+
+
+// Q3
+
+// Q4
+
+// Q5
+// var english = 100
+// var engmarks = 54
+// var math = 100 
+// var mathmarks = 54
+// var urdu = 100
+// var urdumarks = 48
+
+
+// console.log((engmarks*100)/100);
+// console.log((mathmarks*100)/100);
+// console.log((urdumarks*100)/100);
+// console.log((engmarks+mathmarks+urdumarks)*100/(english+math+urdu));
+
