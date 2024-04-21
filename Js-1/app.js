6d1eb10e14618616029e3b7d7ca72f7106b76f3d
Muhammad Yasir <154784215+YasirBrothers@users.noreply.github.com>
2024-04-21 23:40:22 +0500

    Add files via upload

---
+++ Js-1/app.js
@@ -0,0 +1,92 @@
+// Chapter#1
+
+// Q1
+  //  alert("Asalam-O-Alaikum");
+
+// Q2
+  // alert("Error! Please enter a valid password.");
+
+// Q3
+//  alert("Welcome to Js Land... \n Happy Coding!");
+
+// Q4
+//  alert("Welcome to JS Land...");
+
+// alert("Happy Coding! \n Prevent the page from creating additional dialogs.")
+    
+// Q5
+  //  console.log("Hello... I can run JS though my web browser's console");
+  //  alert("Hello... I can run JS though my web browser's console")
+
+//  Q6
+// alert("Welcome to our HTML page");
+
+// Q7
+
+// Chapter#2
+
+// Q1
+// var username = "Muhammad Yasir";
+//  alert(username);
+    
+// Q2
+// var myname = "Muhammad Yasir";
+// alert(myname);
+
+// Q3
+// var message = "Hello World";
+// alert(message);
+
+// var abc = "15 years old";
+//  alert(abc);
+
+// Q4
+// var name = "Jhone Doe";
+//  alert(name);
+
+
+// var age = "15 years old";
+//  alert(age);
+
+// var certified = "Certified Web & Mobile App Development";
+// alert(certified);
+
+// Q5
+    
+// var abc = "PIZZA"
+// alert(abc+"\n"+"PIZZ"+"\n"+"PIZ"+"\n"+"PI"+"\n"+"P" )
+
+// Q6
+// var email = " My email address is yasirtahir009@gmail.com";
+// alert(email);
+
+// Q7
+  // var book = "I am trying to learn from the Book A smarter \n way to learn JavaScript";
+  // alert(book);
+
+  // Q8
+  // document.write("Yah! I can write HTML content through JavaScript ")
+
+  // Q9
+  // alert("----------Java Script----------")
+
+  // Chapter#3
+
+  // Q1
+  // var age =22 ;
+  // alert("I am "+age+" years old");
+
+  // Q2
+  //  var no =14 ;
+  //  alert("You have visited this site "+no+" times");
+
+  // Q3
+  //  var birthyear = 1990;
+  // document.write("My birth year is " + birthyear + " \n Data type of my ddcleared variable is number ");
+
+  // Q4
+  // var name = "Muhammad Yasir";
+  // var title = "XYZ Clothing store";
+  // var quantity = "5";
+  // document.write(" "+name+" ordered "+quantity+" T-shirt(s) on "+title+" ")
+  
